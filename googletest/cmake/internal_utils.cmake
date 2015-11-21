ca8e65d7d7a8d58c59860952ea9c1178d5e8baf7
Pavel Krajcevski <Krajcevski@gmail.com>
2015-11-21 13:23:44 -0500

    Don't 'fix' default compiler settings for MSVC...

--- googletest/cmake/internal_utils.cmake
+++ googletest/cmake/internal_utils.cmake
@@ -51,7 +51,7 @@ macro(config_compiler_and_linker)
     find_package(Threads)
   endif()
 
-  fix_default_compiler_settings_()
+  # fix_default_compiler_settings_()
   if (MSVC)
     # Newlines inside flags variables break CMake's NMake generator.
     # TODO(vladl@google.com): Add -RTCs and -RTCu to debug builds.
