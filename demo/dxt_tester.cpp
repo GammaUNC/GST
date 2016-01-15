3a72d60743ebbcccb25836818603e474eea2a6f5
Pavel Krajcevski <Krajcevski@gmail.com>
2016-01-15 01:52:15 -0500

    Write initial decoder -- needs debugging

--- demo/dxt_tester.cpp
+++ demo/dxt_tester.cpp
@@ -84,6 +84,7 @@ uint32_t Cvt565(uint16_t x) {
   return 0xFF000000 | (b << 16) | (g << 8) | r;
 }
 
+static const uint32_t kNumStreams = 16;
 void encode(const cv::Mat &img, std::vector<uint8_t> *result) {
   // Collect stats for frequency analysis
   std::vector<int16_t> coeffs(img.rows * img.cols, 0);
@@ -129,16 +130,28 @@ void encode(const cv::Mat &img, std::vector<uint8_t> *result) {
   std::vector<int> encoded_counts;
   encoded_counts.reserve(256);
 
-  uint32_t bytes_written = result->size();
   uint32_t sym_idx = 0;
   for (uint32_t i = 0; i < 256; ++i) {
     if (counts[i] == 0) {
       continue;
     }
 
-    while (bytes_written + 3 > result->size()) {
-      result->resize(std::max<uint32_t>(1, result->size() * 2));
+    encoded_symbols[i] = sym_idx++;
+    encoded_counts.push_back(counts[i]);
+  }
+
+  size_t bytes_written = result->size();
+  result->resize(bytes_written + 2);
+  *reinterpret_cast<uint16_t *>(result->data() + bytes_written) =
+    static_cast<uint16_t>(encoded_counts.size());
+  bytes_written += 2;
+
+  for (uint32_t i = 0; i < 256; ++i) {
+    if (counts[i] == 0) {
+      continue;
     }
+
+    result->resize(bytes_written + 3);
 
     uint8_t *symbol_ptr = result->data() + bytes_written;
     uint16_t *count_ptr = reinterpret_cast<uint16_t *>(symbol_ptr + 1);
@@ -146,9 +159,6 @@ void encode(const cv::Mat &img, std::vector<uint8_t> *result) {
     *symbol_ptr = static_cast<uint8_t>(i);
     *count_ptr = static_cast<uint16_t>(counts[i]);
     bytes_written += 3;
-
-    encoded_symbols[i] = sym_idx++;
-    encoded_counts.push_back(counts[i]);
   }
 
   std::cout << std::endl << "Total symbols: " << img.cols * img.rows << std::endl;
@@ -167,7 +177,6 @@ void encode(const cv::Mat &img, std::vector<uint8_t> *result) {
 
   // Write counts to output
   std::vector<ans::OpenCLEncoder> encoders;
-  static const uint32_t kNumStreams = 16;
   encoders.reserve(kNumStreams);
   for (uint32_t i = 0; i < kNumStreams; ++i) {
     encoders.push_back(std::move(ans::OpenCLEncoder(encoded_counts)));
@@ -183,6 +192,16 @@ void encode(const cv::Mat &img, std::vector<uint8_t> *result) {
       // Make sure that we have at least 4*kNumStreams bytes available
       encoded.resize(encoded_bytes_written + (4*kNumStreams));
 
+      int16_t *output = reinterpret_cast<int16_t *>(encoded.data() + encoded_bytes_written);
+      for (uint32_t strm_idx = 0; strm_idx < kNumStreams; strm_idx++) {
+        uint32_t sidx = symbol_offset + strm_idx * ans::kNumEncodedSymbols + sym_idx;
+        if (symbols[sidx] == 0) {
+          *output = coeffs[sym_idx];
+          output++;
+          encoded_bytes_written += 2;
+        }
+      }
+
       for (uint32_t strm_idx = 0; strm_idx < kNumStreams; ++strm_idx) {
         ans::BitWriter w(encoded.data() + encoded_bytes_written);
         uint32_t sidx = symbol_offset + strm_idx * ans::kNumEncodedSymbols + sym_idx;
@@ -193,16 +212,6 @@ void encode(const cv::Mat &img, std::vector<uint8_t> *result) {
 
         encoders[strm_idx].Encode(symbol, w);
         encoded_bytes_written += w.BytesWritten();
-      }
-
-      int16_t *output = reinterpret_cast<int16_t *>(encoded.data() + encoded_bytes_written);
-      for (uint32_t strm_idx = 0; strm_idx < kNumStreams; strm_idx++) {
-        uint32_t sidx = symbol_offset + strm_idx * ans::kNumEncodedSymbols + sym_idx;
-        if (symbols[sidx] == 0) {
-          *output = coeffs[sym_idx];
-          output++;
-          encoded_bytes_written += 2;
-        }
       }
     }
 
@@ -218,10 +227,9 @@ void encode(const cv::Mat &img, std::vector<uint8_t> *result) {
     // Add the offset to the stream...
     uint32_t offset = encoded_bytes_written - last_encoded_bytes_written;
     result->resize(bytes_written + 2);
-    uint16_t *offset_ptr = reinterpret_cast<uint16_t *>(result->data() + bytes_written);
-    assert(offset <= ((1 << 16) - 1));
-    *offset_ptr = static_cast<uint16_t>(offset);
+    *reinterpret_cast<uint16_t *>(result->data() + bytes_written) = static_cast<uint16_t>(offset);
     bytes_written += 2;
+    assert(offset <= ((1 << 16) - 1));
     last_encoded_bytes_written = encoded_bytes_written;
 
     // Advance the symbol offset...
@@ -233,27 +241,27 @@ void encode(const cv::Mat &img, std::vector<uint8_t> *result) {
   memcpy(result->data() + bytes_written, encoded.data(), encoded_bytes_written);
 }
 
+static const cv::Mat quant_table_luma = (cv::Mat_<int16_t>(8, 8) <<
+  16, 11, 10, 16, 24, 40, 51, 61,
+  12, 12, 14, 19, 26, 58, 60, 55,
+  14, 13, 16, 24, 40, 57, 69, 56,
+  14, 17, 22, 29, 51, 87, 80, 62,
+  18, 22, 37, 56, 68, 109, 103, 77,
+  24, 35, 55, 64, 81, 104, 113, 92,
+  49, 64, 78, 87, 103, 121, 120, 101,
+  72, 92, 95, 98, 112, 100, 103, 99);
+
+static const cv::Mat quant_table_chroma = (cv::Mat_<int16_t>(8, 8) <<
+  17, 18, 24, 47, 99, 99, 99, 99,
+  18, 21, 26, 66, 99, 99, 99, 99,
+  24, 26, 56, 99, 99, 99, 99, 99,
+  47, 66, 99, 99, 99, 99, 99, 99,
+  99, 99, 99, 99, 99, 99, 99, 99,
+  99, 99, 99, 99, 99, 99, 99, 99,
+  99, 99, 99, 99, 99, 99, 99, 99,
+  99, 99, 99, 99, 99, 99, 99, 99);
+
 void quantize(cv::Mat *dct, bool is_chroma) {
-  static cv::Mat quant_table_luma = (cv::Mat_<int16_t>(8, 8) <<
-    16, 11, 10, 16, 24, 40, 51, 61,
-    12, 12, 14, 19, 26, 58, 60, 55,
-    14, 13, 16, 24, 40, 57, 69, 56,
-    14, 17, 22, 29, 51, 87, 80, 62,
-    18, 22, 37, 56, 68, 109, 103, 77,
-    24, 35, 55, 64, 81, 104, 113, 92,
-    49, 64, 78, 87, 103, 121, 120, 101,
-    72, 92, 95, 98, 112, 100, 103, 99);
-
-  static cv::Mat quant_table_chroma = (cv::Mat_<int16_t>(8, 8) <<
-    17, 18, 24, 47, 99, 99, 99, 99,
-    18, 21, 26, 66, 99, 99, 99, 99,
-    24, 26, 56, 99, 99, 99, 99, 99,
-    47, 66, 99, 99, 99, 99, 99, 99,
-    99, 99, 99, 99, 99, 99, 99, 99,
-    99, 99, 99, 99, 99, 99, 99, 99,
-    99, 99, 99, 99, 99, 99, 99, 99,
-    99, 99, 99, 99, 99, 99, 99, 99);
-
   for (int j = 0; j < dct->rows / 8; ++j) {
     for (int i = 0; i < dct->cols / 8; ++i) {
       cv::Rect_<int> window(i * 8, j * 8, 8, 8);
@@ -268,6 +276,25 @@ void quantize(cv::Mat *dct, bool is_chroma) {
   static int quantized_copy = 0;
   char fname[256];
   sprintf(fname, "quantized_%d.png", quantized_copy);
+  cv::imwrite(fname, *dct);
+  quantized_copy++;
+}
+
+void dequantize(cv::Mat *dct, bool is_chroma) {
+  for (int j = 0; j < dct->rows / 8; ++j) {
+    for (int i = 0; i < dct->cols / 8; ++i) {
+      cv::Rect_<int> window(i * 8, j * 8, 8, 8);
+      cv::Mat block = (*dct)(window).clone();
+
+      cv::multiply(block, is_chroma ? quant_table_chroma : quant_table_luma, block);
+
+      block.copyTo((*dct)(window));
+    }
+  }
+
+  static int quantized_copy = 0;
+  char fname[256];
+  sprintf(fname, "dequantized_%d.png", quantized_copy);
   cv::imwrite(fname, *dct);
   quantized_copy++;
 }
@@ -316,9 +343,124 @@ std::vector<uint8_t> compress(const cv::Mat &img) {
   return std::move(result);
 }
 
+int decode(cv::Mat *result, const uint8_t *buf) {
+  int offset = 0;
+
+  uint16_t num_symbols = *reinterpret_cast<const uint16_t *>(buf + offset);
+  offset += 2;
+
+  std::vector<uint8_t> symbols(num_symbols, 0);
+  std::vector<int> counts(num_symbols, 0);
+
+  for (uint32_t i = 0; i < num_symbols; ++i) {
+    symbols[i] = *reinterpret_cast<const uint8_t *>(buf + offset);
+    counts[i] = static_cast<int>(*reinterpret_cast<const uint16_t *>(buf + offset + 1));
+    offset += 3;
+  }
+
+  int num_macroblocks = (result->cols * result->rows) / (kNumStreams * ans::kNumEncodedSymbols);
+  assert(num_macroblocks * kNumStreams * ans::kNumEncodedSymbols == result->cols * result->rows);
+  std::vector<uint16_t> macroblock_offsets(num_macroblocks);
+
+  for (int i = 0; i < num_macroblocks; ++i) {
+    macroblock_offsets[i] = *reinterpret_cast<const uint16_t *>(buf + offset);
+    offset += 2;
+  }
+
+  std::vector<int16_t> coeffs(result->cols * result->rows);
+
+  int symbol_offset = 0;
+  int last_offset = 0;
+  for (uint16_t mb_off : macroblock_offsets) {
+    int mb_size = mb_off - last_offset;
+
+    const uint32_t *states = reinterpret_cast<const uint32_t *>(buf + offset + mb_off) - kNumStreams;
+    std::vector<ans::OpenCLCPUDecoder> decoders;
+    decoders.reserve(kNumStreams);
+    for (uint32_t i = 0; i < kNumStreams; ++i) {
+      decoders.push_back(ans::OpenCLCPUDecoder(states[kNumStreams - i - 1], counts));
+    }
+
+    int data_sz_bytes = mb_size - 4 * kNumStreams;
+    assert(data_sz_bytes % 2 == 0);
+
+    std::vector<uint16_t> mb_data(data_sz_bytes / 2);
+    memcpy(mb_data.data(), buf + offset + last_offset, data_sz_bytes);
+    std::reverse(mb_data.begin(), mb_data.end());
+
+    ans::BitReader r(reinterpret_cast<const uint8_t *>(mb_data.data()));
+    for (int sym_idx = 0; sym_idx < ans::kNumEncodedSymbols; sym_idx++) {
+      for (int strm_idx = 0; strm_idx < kNumStreams; ++strm_idx) {
+        uint32_t sidx = symbol_offset + (strm_idx + 1) * ans::kNumEncodedSymbols - sym_idx - 1;
+        coeffs[sidx] = static_cast<int16_t>(decoders[strm_idx].Decode(r)) - 128;
+      }
+
+      size_t coeffs_sz = coeffs.size();
+      for (int strm_idx = 0; strm_idx < kNumStreams; ++strm_idx) {
+        uint32_t idx = symbol_offset + (strm_idx + 1) * ans::kNumEncodedSymbols - sym_idx - 1;
+        if (coeffs[idx] == -128) {
+          coeffs[idx] = static_cast<int16_t>(r.ReadBits(16));
+        }
+      }
+    }
+
+    last_offset = mb_off;
+    symbol_offset += kNumStreams * ans::kNumEncodedSymbols;
+  }
+
+  offset += macroblock_offsets[num_macroblocks - 1];
+
+  // Populate the image properly
+  assert(result->type() == CV_16SC1);
+  uint32_t coeff_idx = 0;
+  for (int j = 0; j < result->rows; ++j) {
+    for (int i = 0; i < result->cols; ++i) {
+      result->at<int16_t>(j, i) = coeffs[coeff_idx++];
+    }
+  }
+
+  return offset;
+}
+
+int decompressChannel(cv::Mat *result, const uint8_t *buf, int width, int height, bool is_chroma) {
+  int num_symbols = width * height;
+
+  *result = cv::Mat(height, width, CV_16SC1);
+  int offset = decode(result, buf);
+
+  dequantize(result, is_chroma);
+  dct::RunIDCT(result);
+
+  return offset;
+}
+
 cv::Mat decompress(const std::vector<uint8_t> &stream) {
-  assert(!"unimplemented");
-  return cv::Mat();
+  const uint8_t *stream_buf = stream.data();
+
+  const uint32_t *size_buf = reinterpret_cast<const uint32_t *>(stream_buf);
+  int width = size_buf[0];
+  int height = size_buf[1];
+
+  int num_symbols_luma = width * height;
+  int num_symbols_chroma = ((width + 1) / 2) * ((height + 1) / 2);
+
+  int offset = 8;
+
+  cv::Mat channels[3];
+  offset += decompressChannel(channels + 0, stream_buf + offset, width, height, false);
+  offset += decompressChannel(channels + 1, stream_buf + offset, (width + 1) / 2, (height + 1) / 2, true);
+  offset += decompressChannel(channels + 2, stream_buf + offset, (width + 1) / 2, (height + 1) / 2, true);
+
+  // Resize chroma to full...
+  cv::resize(channels[1], channels[1], cv::Size(width, height));
+  cv::resize(channels[2], channels[2], cv::Size(width, height));
+
+  cv::Mat img_YCrCb;
+  cv::merge(channels, 3, img_YCrCb);
+
+  cv::Mat result;
+  cv::cvtColor(img_YCrCb, result, CV_YCrCb2RGB);
+  return result;
 }
 
 cv::Mat codec(const cv::Mat &img) {
@@ -422,6 +564,9 @@ int main(int argc, char **argv) {
 
   cv::Mat decomp_A = codec(img_A);
   cv::Mat decomp_B = codec(img_B);
+
+  cv::imwrite("dxt_img_codec_A.png", decomp_A);
+  cv::imwrite("dxt_img_codec_B.png", decomp_B);
 
   cv::Mat img_A_YCrCb, img_B_YCrCb;
   cv::cvtColor(img_A, img_A_YCrCb, CV_RGB2YCrCb);
