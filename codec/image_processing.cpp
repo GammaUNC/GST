343f85d5edff6f59ff855729d1be9f55c7409ffa
Pavel Krajcevski <pavel@cs.unc.edu>
2016-02-06 17:59:05 -0500

    Move 565 expansion into image processing

--- codec/image_processing.cpp
+++ codec/image_processing.cpp
@@ -54,4 +54,30 @@ YCrCbtoRGB::Run(const typename YCrCbtoRGB::Base::ArgType &in) const {
   return std::move(std::unique_ptr<RGBImage>(img));
 }
 
+std::unique_ptr<RGBImage> Expand565::Run(const std::unique_ptr<RGB565Image> &in) const {
+  const size_t w = in->Width();
+  const size_t h = in->Height();
+
+  std::vector<uint8_t> result;
+  result.reserve(w * h * 3);
+
+  for (size_t j = 0; j < h; ++j) {
+    for (size_t i = 0; i < w; ++i) {
+      auto pixel = in->GetAt(i, j);
+
+      uint32_t r = (pixel[0] << 3) | (pixel[0] >> 2);
+      result.push_back(static_cast<uint8_t>(r));
+
+      uint32_t g = (pixel[1] << 2) | (pixel[1] >> 4);
+      result.push_back(static_cast<uint8_t>(g));
+
+      uint32_t b = (pixel[2] << 3) | (pixel[2] >> 2);
+      result.push_back(static_cast<uint8_t>(b));
+    }
+  }
+
+  RGBImage *img = new RGBImage(w, h, std::move(result));
+  return std::move(std::unique_ptr<RGBImage>(img));
+}
+
 }  // namespace GenTC
--- codec/image_processing.h
+++ codec/image_processing.h
@@ -9,13 +9,23 @@ namespace GenTC {
 class RGBtoYCrCb: public PipelineUnit<RGBImage, YCbCrImage> {
  public:
   typedef PipelineUnit<RGBImage, YCbCrImage> Base;
+  static std::unique_ptr<Base> New() { return std::unique_ptr<Base>(new RGBtoYCrCb); }
   typename Base::ReturnType Run(const typename Base::ArgType &) const override;
 };
 
 class YCrCbtoRGB: public PipelineUnit<YCbCrImage, RGBImage> {
  public:
   typedef PipelineUnit<YCbCrImage, RGBImage> Base;
+  static std::unique_ptr<Base> New() { return std::unique_ptr<Base>(new YCrCbtoRGB); }
   typename Base::ReturnType Run(const typename Base::ArgType &) const override;
+};
+
+class Expand565
+: public PipelineUnit<RGB565Image, RGBImage> {
+ public:
+  typedef PipelineUnit<RGB565Image, RGBImage> Base;
+  static std::unique_ptr<Base> New() { return std::unique_ptr<Base>(new Expand565); }
+  std::unique_ptr<RGBImage> Run(const std::unique_ptr<RGB565Image> &) const override;
 };
 
 }  // namespace GenTC
