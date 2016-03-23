f7a9be3597d62e49493efbe8c518bea43ac815eb
Pavel Krajcevski <Krajcevski@gmail.com>
2016-03-23 12:55:53 -0400

    Move specialized ans decoding to codec

--- codec/codec.cpp
+++ codec/codec.cpp
@@ -9,7 +9,141 @@
 
 #include <iostream>
 
+#include "ans_config.h"
 #include "ans_ocl.h"
+#include "gpu.h"
+
+using gpu::GPUContext;
+
+static const size_t kWaveletBlockDim = 32;
+
+static inline cl_mem_flags GetHostReadOnlyFlags() {
+#ifdef CL_VERSION_1_2
+  return CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR | CL_MEM_HOST_NO_ACCESS;
+#else
+  return CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR;
+#endif
+}
+
+struct AnsTableEntry {
+  cl_ushort freq;
+  cl_ushort cum_freq;
+  cl_uchar  symbol;
+};
+
+struct CLKernelResult {
+  cl_mem output;
+  cl_event output_event;
+};
+
+static CLKernelResult DecodeANS(GPUContext *gpu_ctx, const cl_uchar *data, const size_t num_symbols) {
+  cl_kernel build_table_kernel = gpu_ctx->GetOpenCLKernel(
+    ans::kANSOpenCLKernels[ans::eANSOpenCLKernel_BuildTable], "build_table");
+
+  cl_context ctx = gpu_ctx->GetOpenCLContext();
+
+  // First get the number of frequencies...
+  cl_uint num_freqs = static_cast<cl_uint>(data[0]);
+  data++;
+
+  // Load all of the frequencies
+  cl_uint freqs[256];
+  for (cl_uint i = 0; i < num_freqs; ++i) {
+    memcpy(freqs + i, data, sizeof(*freqs));
+    data += sizeof(*freqs);
+  }
+
+  cl_mem_flags flags = GetHostReadOnlyFlags();
+
+  // Note: we could do this on the GPU as well, but the array size here is almost never more than
+  // about 256, so the CPU is actually much better at doing it. We can also stick it in constant
+  // memory, which makes the upload not that bad...
+  cl_uint cum_freqs[256];
+  std::partial_sum(freqs, freqs + num_freqs - 1, cum_freqs + 1);
+
+  size_t M = static_cast<size_t>(cum_freqs[num_freqs - 1] + freqs[num_freqs]);
+
+  cl_int errCreateBuffer;
+  cl_mem table = clCreateBuffer(gpu_ctx->GetOpenCLContext(),
+    CL_MEM_READ_WRITE, M * sizeof(AnsTableEntry), NULL, &errCreateBuffer);
+  CHECK_CL((cl_int), errCreateBuffer);
+
+  cl_mem freqs_buffer = clCreateBuffer(ctx, flags, num_freqs * sizeof(freqs[0]), freqs, &errCreateBuffer);
+  CHECK_CL((cl_int), errCreateBuffer);
+
+  const size_t cum_freqs_buf_size = num_freqs * sizeof(cum_freqs[0]);
+  cl_mem cum_freqs_buffer = clCreateBuffer(ctx, flags, cum_freqs_buf_size, cum_freqs, &errCreateBuffer);
+  CHECK_CL((cl_int), errCreateBuffer);
+
+  CHECK_CL(clSetKernelArg, build_table_kernel, 0, sizeof(freqs_buffer), &freqs_buffer);
+  CHECK_CL(clSetKernelArg, build_table_kernel, 1, sizeof(cum_freqs_buffer), &cum_freqs_buffer);
+  CHECK_CL(clSetKernelArg, build_table_kernel, 2, sizeof(cl_uint), &num_freqs);
+  CHECK_CL(clSetKernelArg, build_table_kernel, 3, sizeof(table), &table);
+
+  cl_event build_table_event;
+  CHECK_CL(clEnqueueNDRangeKernel, gpu_ctx->GetCommandQueue(), build_table_kernel,
+                                   1, NULL, &M, NULL, 0, NULL, &build_table_event);
+
+  CHECK_CL(clReleaseMemObject, freqs_buffer);
+  CHECK_CL(clReleaseMemObject, cum_freqs_buffer);
+
+  cl_kernel decode_kernel = gpu_ctx->GetOpenCLKernel(
+    ans::kANSOpenCLKernels[ans::eANSOpenCLKernel_ANSDecode], "ans_decode");
+
+  // First, just set our table buffers...
+  CHECK_CL(clSetKernelArg, decode_kernel, 0, sizeof(table), &table);
+
+  // Load all of the offsets to the different data streams...
+  cl_uint num_offsets = static_cast<cl_uint>(data[0]);
+  data++;
+
+#ifndef NDEBUG
+  // Make sure that each offset is a multiple of four, otherwise we won't be getting
+  // the values we're expecting in our kernel.
+  const cl_uint *debug_offsets = reinterpret_cast<const cl_uint *>(data);
+  for (cl_uint i = 0; i < num_offsets; ++i) {
+    assert((debug_offsets[i] & 0x3) == 0);
+  }
+#endif
+
+  cl_uint data_sz = reinterpret_cast<const cl_uint *>(data)[num_offsets - 1];
+  cl_mem data_buf = clCreateBuffer(ctx, GetHostReadOnlyFlags(), data_sz,
+    const_cast<cl_uchar *>(data), &errCreateBuffer);
+  CHECK_CL((cl_int), errCreateBuffer);
+  CHECK_CL(clSetKernelArg, decode_kernel, 1, sizeof(data_buf), &data_buf);
+
+#ifdef CL_VERSION_1_2
+  cl_mem_flags out_flags = CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY;
+#else
+  cl_mem_flags out_flags = CL_MEM_WRITE_ONLY;
+#endif
+
+  // Allocate 256 * num interleaved slots for result
+  size_t total_encoded =
+    num_offsets * ans::ocl::kThreadsPerEncodingGroup * ans::ocl::kNumEncodedSymbols;
+  const size_t total_streams = total_encoded / ans::ocl::kNumEncodedSymbols;
+  const size_t streams_per_work_group = ans::ocl::kThreadsPerEncodingGroup;
+
+  CLKernelResult result;
+  result.output = clCreateBuffer(ctx, out_flags, total_encoded, NULL, &errCreateBuffer);
+  CHECK_CL((cl_int), errCreateBuffer);
+  CHECK_CL(clSetKernelArg, decode_kernel, 2, sizeof(result.output), &result.output);
+
+  CHECK_CL(clEnqueueNDRangeKernel, gpu_ctx->GetCommandQueue(), decode_kernel,
+                                   1, NULL, &total_streams, &streams_per_work_group,
+                                   1, &build_table_event, &result.output_event);
+
+  CHECK_CL(clReleaseMemObject, table);
+  CHECK_CL(clReleaseMemObject, data_buf);
+
+  return result;
+}
+
+static CLKernelResult InverseWavelet(GPUContext *gpu_ctx, CLKernelResult img, int width, int height) {
+
+  CLKernelResult result;
+  return result;
+}
 
 namespace GenTC {
 
@@ -25,10 +159,10 @@ RunDXTEndpointPipeline(const std::unique_ptr<Image<T> > &img) {
     WaveletUnsignedTy;
 
   auto pipeline = Pipeline<Image<T>, Image<WaveletSignedTy> >
-    ::Create(FWavelet2D<T, 32>::New())
+    ::Create(FWavelet2D<T, kWaveletBlockDim>::New())
     ->Chain(MakeUnsigned<WaveletSignedTy>::New())
     ->Chain(Linearize<WaveletUnsignedTy>::New())
-    ->Chain(RearrangeStream<WaveletUnsignedTy>::New(img->Width(), 32))
+    ->Chain(RearrangeStream<WaveletUnsignedTy>::New(img->Width(), kWaveletBlockDim))
     ->Chain(ReducePrecision<WaveletUnsignedTy, uint8_t>::New())
     ->Chain(ByteEncoder::Encoder(ans::ocl::kNumEncodedSymbols));
 
